6a2feb1679e5c525f6bc0bea4ca355ea9eeea077
TanujaAtole <atoletanuja1@gmail.com>
2025-01-23 17:11:25 +0530

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,17 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+  function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+  }
+
+
+
+  window.addEventListener("load" , typewriter);
+
